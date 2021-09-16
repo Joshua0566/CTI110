ec8140696443e581b4b535e81e88d4097532aeef
Joshua0566 <90853011+Joshua0566@users.noreply.github.com>
2021-09-16 11:08:29 -0400

    First upload

---
+++ P1Lab2.py
@@ -0,0 +1,11 @@
+# CTI #110
+# P1LAB1
+# Name
+# Date           
+
+#7.12 zybooks
+
+userNum = int(input())
+userNumSquared = userNum * userNum   # Bug fixed
+   
+print(userNumSquared)       # Output formatting issue here; fix it when instructed
